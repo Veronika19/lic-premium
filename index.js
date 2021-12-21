c834b5adeb9465edd45da563cc42d653fcf2aa02
Pankaj Kumar <pankaj151290mech@gmail.com>
2021-12-21 18:37:01 +0530

    remvd path module error

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, './client/build')));
 
   // serve index.html i any route is not found
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
